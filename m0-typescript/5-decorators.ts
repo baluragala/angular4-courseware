8193cf9f19b2a6825ff87d26f57f6b3aca08ca38
Balakrishna Ragala <baluragala@gmail.com>
2017-09-18 19:58:27 +0530

    Update 5-decorators.ts

--- m0-typescript/5-decorators.ts
+++ m0-typescript/5-decorators.ts
@@ -1,8 +1,52 @@
-function SimpleDecorator(constructor: Function) {
+/* class decorator */
+function simpleDecorator(constructor: Function) {
+    console.log('********CLASS**************')
+    console.log('constructor', constructor);
     console.log('simpleDecorator called.');
 }
 
-@SimpleDecorator
-class ClassWithSimpleDecorator {
+/*method decorator*/
+function log(target: any,
+    methodName: string,
+    descriptor?: PropertyDescriptor) {
+    
+    let originalFunction = target[methodName];
 
+    let auditFunction = function () {
+        console.log(`${methodName} called with args : ${JSON.stringify(arguments)}`);
+        originalFunction.apply(this, arguments);
+    }
+
+    target[methodName] = auditFunction;
 }
+
+/* property decorator */
+function propertyDec(target: any, propertyKey: string) {
+    console.log('********PROPERTY**************')
+    console.log(`target : ${target}`);
+    console.log(`target.constructor : ${target.constructor}`);
+    console.log(`class name : `
+        + `${target.constructor.name}`);
+    console.log(`propertyKey : ${propertyKey}`);
+    console.log('**********************')
+}
+
+
+class Person1 {
+
+
+    fullName:string;
+
+    @log
+    //some line here
+    print(fullName: string) {
+        console.log(`Person.print`
+            + `(${fullName}) called.`);
+    }
+    //some line here
+}
+
+let p1 = new Person1();
+p1.print("Bala Krishna"); 
+p1.print("Rama Krishna"); 
+p1.print("Hari Krishna"); 
